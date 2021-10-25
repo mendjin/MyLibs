bf4b023aaae8d82becac22dbce3dd29d90838b15
mendjin michelle <mendjinmichelle@gmail.com>
2021-10-25 13:26:09 +0200

    add serialize and deserialise method

--- MyLibs.Serialisation/Serializer.cs
+++ MyLibs.Serialisation/Serializer.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.IO;
 
 namespace MyLibs.Serialisation
 {
     public class Serializer
     {
+        public void Serialize<T>(T data,string path )
+        {
+          using(StreamWriter file =new StreamWriter(path,true) )
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.serialize(file, data);
+
+
+
+            }
+        }
+        public T Deserialize <T>(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+               return T bf.Deserialize(file, data);
+            }
     }
 }
